8e5255f2b385f0640ee072cd80791dbcb88cad89
hbsnow <yuki.4uing@gmail.com>
2021-06-28 21:49:14 +0900

    fix: remove comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,43 +23,6 @@ const config = {
       },
     ],
   },
-  // overrides: [
-  //   {
-  //     files: ["**/*.{ts,tsx}"],
-  //     extends: [
-  //       "eslint:recommended",
-  //       "plugin:react/recommended",
-  //       "plugin:@typescript-eslint/recommended",
-  //       "prettier",
-  //     ],
-  //     parser: "@typescript-eslint/parser",
-  //     settings: {
-  //       react: {
-  //         pragma: "React",
-  //         version: "detect",
-  //       },
-  //     },
-  //     parserOptions: {
-  //       sourceType: "module",
-  //       project: "./tsconfig.json",
-  //       ecmaFeatures: {
-  //         jsx: true,
-  //       },
-  //     },
-  //     plugins: ["@typescript-eslint", "react", "react-hooks", "import"],
-  //     rules: {
-  //       "no-unused-vars": "off",
-  //       "@typescript-eslint/no-unused-vars": "error",
-  //       "react/jsx-no-target-blank": "error",
-  //       "react/prop-types": "off",
-  //       "react-hooks/rules-of-hooks": "error",
-  //       "react-hooks/exhaustive-deps": "error",
-  //     },
-  //   },
-  // ],
-  // globals: {
-  //   React: "writable",
-  // },
 };
 
 module.exports = config;
