208bf0031186e5ed01caa28de8ba2eb5c386f271
hbsnow <yuki.4uing@gmail.com>
2021-07-07 20:38:20 +0900

    feat: purge html

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   mode: "jit",
-  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  purge: ["./public/**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
