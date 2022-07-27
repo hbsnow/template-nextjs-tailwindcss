4b4599f95d9589d8bf7cea5073933fcc960be627
hbsnow <yuki.4uing@gmail.com>
2022-07-27 16:09:45 +0900

    fix: vitest to jest

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,16 +1,22 @@
-module.exports = {
+import type { StorybookConfig } from "@storybook/core-common";
+
+const config: StorybookConfig = {
   framework: "@storybook/react",
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    "storybook-addon-next",
   ],
   core: {
-    builder: "storybook-builder-vite",
+    builder: "webpack5",
   },
   features: {
     storyStoreV7: true,
     interactionsDebugger: true,
   },
 };
+
+export default config;
